d8fcf63eaeee1802451093eb2a09fad202182b58
Ankit <ankitrajdihara123@gmail.com>
2026-09-18 23:58:06 +0530

    fial_the_secondz_draftapp/components/PageLayout.jsx

--- app/components/PageLayout.jsx
+++ app/components/PageLayout.jsx
@@ -4,7 +4,6 @@ import {Aside} from '~/components/Aside';
 import {Footer} from '~/components/Footer';
 import {Header, HeaderMenu} from '~/components/Header';
 import {CartMain} from '~/components/CartMain';
-import {FlyingBee} from '~/components/FlyingBee';
 import {
   SEARCH_ENDPOINT,
   SearchFormPredictive,
@@ -41,7 +40,6 @@ export function PageLayout({
         header={header}
         publicStoreDomain={publicStoreDomain}
       />
-      <FlyingBee />
     </Aside.Provider>
   );
 }
